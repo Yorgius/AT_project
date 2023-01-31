ae9d2d9d47cc70860f8f602352539e4dd0ca9bfd
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-31 21:36:27 +0300

    transfer highcharts configuration in to the view

--- yarn_prices/views.py
+++ yarn_prices/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, HttpResponse
+import json
 
-# Create your views here.
+
+# main page
 def show_home_page(request):
     context = [{'title': 'Home'}]
     return render(request, 'yarn_prices/home.html', )
 
-
-def show_price_charts(request):
-    return HttpResponse('Charts page')
-
+# визуализация полученных данных в HighCharts
 def show_charts_page(request):
-    return render(request, 'yarn_prices/charts.html')
+
+    data1 = {
+        'name': 'data1',
+        'data': [['Tokyo', 37.33], ['Delhi', 31.18], ['Shanghai', 27.79], ['Sao Paulo', 25.23]],
+        'color': 'red'
+    }
+
+    chart = {
+        'chart': {'type': 'column'},
+        'legend': {'enabled': True},
+        'title': {'text': 'Сравнение цен на пряжу в г. Минске'},
+        'xAxis': {'categories': ['Tokyo','Delhi','Shanghai','Sao Paulo']},
+        'series': [data1]
+    }
+
+    return render(request, 'yarn_prices/charts.html', {'chart': json.dumps(chart)})
+
