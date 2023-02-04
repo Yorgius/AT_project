7e2000a85e987fb39413f17d989277517ec4e357
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-02-04 09:48:18 +0300

    added simple models for DB

--- yarn_prices/models.py
+++ yarn_prices/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    url = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Yarn(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    price = models.FloatField(blank=True)
+    date_added = models.DateField(auto_now_add=True)
+    shop = models.ForeignKey('Shop', on_delete=models.PROTECT, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
