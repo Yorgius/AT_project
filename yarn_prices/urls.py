ef6e5a480607b8453b00aa40bdcb61158dfbd033
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-02-04 13:54:18 +0300

    added view for the charts

--- yarn_prices/urls.py
+++ yarn_prices/urls.py
@@ -7,5 +7,6 @@ app_name = 'yarn_prices_urls'
 # The `urlpatterns` list routes URLs to views.
 urlpatterns = [
     path('', show_home_page, name='home'),
-    path('charts/', show_charts_page, name='charts')
+    path('charts/', show_charts_page, name='charts'),
+    path('loading_data/', save_data_by_bs, name='start_bs'),
 ]
--- yarn_prices/views.py
+++ yarn_prices/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+from datetime import datetime
 import json
+
+from .models import *
+from .scrapers import magic_soup
 
 
 # main page
@@ -9,20 +13,60 @@ def show_home_page(request):
 
 # визуализация полученных данных в HighCharts
 def show_charts_page(request):
-
-    data1 = {
-        'name': 'data1',
-        'data': [['Tokyo', 37.33], ['Delhi', 31.18], ['Shanghai', 27.79], ['Sao Paulo', 25.23]],
-        'color': 'red'
+    # Если в базе нет записи с текущей датой
+    todays_day = datetime.now().date()
+    if not Yarn.objects.filter(date_added=todays_day).exists():
+        return redirect('yarn_prices_urls:start_bs')
+    
+    data_values, categories = [], []
+    data_set = Yarn.objects.select_related('shop').filter(date_added=todays_day).order_by('price')
+    for yarn in data_set:
+        categories.append(yarn.shop.name)
+        data_values.append([yarn.shop.name, yarn.price])
+    # конфигурация графика
+    data = {
+        'name': 'Цена',
+        'data': data_values,
+        'color': '#87CEEB',
+        'dataLabels': {
+            'enabled': True,
+            'color': '#FFFFFF',
+            'align': 'center',
+            'format': '{point.y:.2f}',
+            # 'y': 0,
+            'style': {
+                'fontSize': '10px',
+                'fontFamily': 'Verdana, sans-serif'
+            }
+        }
     }
 
-    chart = {
+    chart = json.dumps({
         'chart': {'type': 'column'},
         'legend': {'enabled': True},
-        'title': {'text': 'Сравнение цен на пряжу в г. Минске'},
-        'xAxis': {'categories': ['Tokyo','Delhi','Shanghai','Sao Paulo']},
-        'series': [data1]
-    }
+        'title': {'text': 'Сравнение цен на пряжу "Детская новинка"'},
+        'subtitle': {'text': 'по РБ'},
+        'xAxis': {'categories': categories},
+        'yAxis': {'min': 0, 'title': {'text': 'BYN'}},
+        'legend': {'enabled': False},
+        'tooltip': {'pointFormat': "Цена: {point.y:.2f}"},
+        'series': [data],
+    })
 
-    return render(request, 'yarn_prices/charts.html', {'chart': json.dumps(chart)})
+    return render(request, 'yarn_prices/charts.html', {'chart': chart})
 
+def save_data_by_bs(request):
+    dataset = magic_soup()
+    for data in dataset:
+        try:
+            if Shop.objects.filter(name=data.get('shop_name')).exists():
+                shop = Shop.objects.get(name=data.get('shop_name'))
+            else:
+                shop = Shop.objects.create(name=data.get('shop_name'),  url=data.get('shop_url'))
+            yarn = Yarn(name=data.get('yarn_name'), price=data.get('yarn_price'), shop=shop)
+            yarn.save()
+        except Exception as exptn:
+            print('Вызвано исключение при сохранении нового объекта в БД')
+            print(f'Exception: {exptn}')
+            return redirect('yarn_prices_urls:home')
+    return redirect('yarn_prices_urls:charts')
